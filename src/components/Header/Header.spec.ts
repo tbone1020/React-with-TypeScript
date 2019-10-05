e5ace0e85a03bdf12d6f70b2a107a9125a97da45
Trent Teebken <trenttee1020@gmail.com>
2019-10-04 19:56:48 -0700

    Added Jasmine unit tests

---
+++ src/components/Header/Header.spec.ts
@@ -0,0 +1,5 @@
+describe('Header', () => {
+  it('can be built', () => {
+
+  })
+})
---
+++ src/components/Users/User.spec.ts
@@ -0,0 +1,9 @@
+import User from './User';
+
+describe('User', () => {
+
+  it('Exists', () => {
+    expect(User).toBeDefined();
+  })
+
+});
